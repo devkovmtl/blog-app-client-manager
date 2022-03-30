a3e9e46b95c0cc94d4e26a23e87a77381a2acdc9
Mikael <dev.kov.mtl@gmail.com>
2022-03-30 11:19:03 -0400

    Setup route and User Provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import { Routes, Route } from 'react-router-dom';
+import UserContext from './context/UserContext';
+import useAuth from './hooks/useAuth';
 import Header from './components/Header';
+import ProtectedRoute from './helpers/ProtectedRoute';
+import UserIsLoggedIn from './helpers/UserIsLoggedIn';
 import Login from './components/Login';
 import PostList from './components/PostList';
 
 function App() {
+  const { auth, saveAuth, removeAuth } = useAuth();
+
+  console.log(auth);
+
   return (
-    <div>
-      <Header />
-      <main>
-        <Routes>
-          <Route path='login' element={<Login />} />
-          <Route path='/' element={<PostList />} />
-        </Routes>
-      </main>
-    </div>
+    <UserContext.Provider value={{ auth, saveAuth, removeAuth }}>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <ProtectedRoute auth={auth} redirectPath='/login'>
+                  <PostList />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/login'
+              element={
+                <UserIsLoggedIn auth={auth} redirectPath='/'>
+                  <Login />
+                </UserIsLoggedIn>
+              }
+            />
+          </Routes>
+        </main>
+      </div>
+    </UserContext.Provider>
   );
 }
 
