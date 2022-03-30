5878538b626597258cf89729b86d9cb30b27b158
Mikael <dev.kov.mtl@gmail.com>
2022-03-30 11:19:39 -0400

    Update style and header to hide nav if not logged

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,42 @@
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import UserContent from '../context/UserContext';
+
 const Header = () => {
+  let navigate = useNavigate();
+  const { auth, removeAuth } = useContext(UserContent);
+
   return (
     <header>
-      <h1 className='title'>Blog - Manager</h1>
+      <h1 className='title' onClick={() => navigate('/')}>
+        Blog - Manager
+      </h1>
       <span className='flex'></span>
-      <nav>
-        <ul className='nav'>
-          <li className='nav__link'>
-            <a href='/' className='nav__link__item active'>
-              Home
-            </a>
-          </li>
-          <li className='nav__link'>
-            <a href='/' className='nav__link__item'>
-              Add Post
-            </a>
-          </li>
-        </ul>
-      </nav>
+
+      {auth && auth.token && (
+        <nav>
+          <ul className='nav'>
+            <li className='nav__link'>
+              <Link to='/' className='nav__link__item active'>
+                Home
+              </Link>
+            </li>
+            <li className='nav__link'>
+              <Link to='/' className='nav__link__item'>
+                Add Post
+              </Link>
+            </li>
+            <li className='nav__link'>
+              <button
+                className='nav__button nav__button--logout'
+                onClick={removeAuth}
+              >
+                Log Out
+              </button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
