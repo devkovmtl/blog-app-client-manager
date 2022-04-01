a92af05367e781bc6df6be69da89fe78a1b51609
Mikael <dev.kov.mtl@gmail.com>
2022-03-31 21:12:50 -0400

    Check if jwt session has expired

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import jwtDecode from 'jwt-decode';
 
 const useAuth = () => {
   const getUser = () => {
@@ -13,10 +14,15 @@ const useAuth = () => {
   };
 
   const [auth, setAuth] = useState(getUser());
+  const [decodedJwtToken, setDecodedJwtToken] = useState();
+  const [timer, setTimer] = useState();
 
   const saveAuth = (credentials) => {
     localStorage.setItem('user', JSON.stringify(credentials.user));
     localStorage.setItem('token', JSON.stringify(credentials.token));
+
+    setDecodedJwtToken(jwtDecode(credentials.token));
+
     setAuth((prevState) => ({
       ...prevState,
       token: credentials.token,
@@ -30,9 +36,26 @@ const useAuth = () => {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
     setAuth(null);
+    setDecodedJwtToken(null);
   };
 
-  return { auth, saveAuth, removeAuth };
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem('token'))) {
+      setDecodedJwtToken(jwtDecode(JSON.parse(localStorage.getItem('token'))));
+    }
+  }, []);
+
+  useEffect(() => {
+    let interval;
+    if (decodedJwtToken) {
+      interval = setInterval(() => {
+        setTimer(decodedJwtToken.exp * 1000 - new Date().getTime());
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [decodedJwtToken, timer]);
+
+  return { auth, saveAuth, removeAuth, timer };
 };
 
 export default useAuth;
